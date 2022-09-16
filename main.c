6c469528b4416f4511a06895de137d376cef13ca
François Basset <mrfrancoisbasset@gmail.com>
2022-09-16 22:29:27 +0200

    I added the file entry in appshortcut struct.

--- main.c
+++ main.c
@@ -7,11 +7,12 @@ int main(void) {
 	struct appshortcut *app_shortcuts = appshortcut_get_app_shortcuts(&length);
 
 	for (int i = 0; i < length; i++) {
-		printf("%s -- %s -- %s -- %s\n", app_shortcuts[i].name, app_shortcuts[i].exec, app_shortcuts[i].categorie, app_shortcuts[i].icon);
+		printf("%s\n%s -- %s -- %s -- %s\n\n", app_shortcuts[i].file, app_shortcuts[i].name, app_shortcuts[i].exec, app_shortcuts[i].categorie, app_shortcuts[i].icon);
 		free(app_shortcuts[i].name);
 		free(app_shortcuts[i].exec);
 		free(app_shortcuts[i].categorie);
 		free(app_shortcuts[i].icon);
+		free(app_shortcuts[i].file);
 	}
 
 	free(app_shortcuts);
